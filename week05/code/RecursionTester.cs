6e032ec1d2a99f5cf0709da8281471f66e340dc1
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-07-22 19:24:26 -0600

    Update RecursionTester.cs

--- week05/code/RecursionTester.cs
+++ week05/code/RecursionTester.cs
@@ -1,40 +1,7 @@
 using System;
-
-public static class RecursionTester
-{
-    /// <summary>
-    /// Entry point for the Prove 8 tests
-    /// </summary>
-    public static void Run()
-    {
-        // Sample Test Cases (may not be comprehensive)
-        Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
-        Console.WriteLine(SumSquaresRecursive(10)); // Expected output: 385
-        Console.WriteLine(SumSquaresRecursive(100)); // Expected output: 338350
-    }
-
-    /// <summary>
-    /// Recursive method to sum the squares of numbers from 1 to n
-    /// </summary>
-    /// <param name="n">The number up to which squares are summed</param>
-    /// <returns>The sum of squares from 1 to n</returns>
-    public static int SumSquaresRecursive(int n)
-    {
-        if (n <= 0)
-            return 0;
-        else
-            return n * n + SumSquaresRecursive(n - 1);
-    }
-}
-
-// To run the tests
-RecursionTester.Run();
-
-//
-using System;
 using System.Collections.Generic;
 
-class Program
+partial class Program
 {
     static void Main()
     {
@@ -58,6 +25,7 @@ class Program
         return ways;
     }
 }
+
 
     
 

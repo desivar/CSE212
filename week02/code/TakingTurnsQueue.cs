9c85b6c074c6767a8148d607172afc51d9ed1788
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-11-18 14:14:10 -0600

    Update TakingTurnsQueue.cs
    fix code

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,10 +1,10 @@
 /// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
+/// This queue is circular. When people are added via AddPerson, then they are added to the 
+/// back of the queue (per FIFO rules). When GetNextPerson is called, the next person
+/// in the queue is saved to be returned and then they are placed back into the back of the queue. Thus,
+/// each person stays in the queue and is given turns. When a person is added to the queue, 
+/// a turns parameter is provided to identify how many turns they will be given. If the turns is 0 or
+/// less than they will stay in the queue forever. If a person is out of turns then they will 
 /// not be added back into the queue.
 /// </summary>
 public class TakingTurnsQueue
@@ -14,10 +14,10 @@ public class TakingTurnsQueue
     public int Length => _people.Length;
 
     /// <summary>
-    /// Add new people to the queue with a name and number of turns
+    /// Add new people to the queue with a name and number of turns.
     /// </summary>
-    /// <param name="name">Name of the person</param>
-    /// <param name="turns">Number of turns remaining</param>
+    /// <param name="name">Name of the person.</param>
+    /// <param name="turns">Number of turns remaining.</param>
     public void AddPerson(string name, int turns)
     {
         var person = new Person(name, turns);
@@ -27,8 +27,8 @@ public class TakingTurnsQueue
     /// <summary>
     /// Get the next person in the queue and return them. The person should
     /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
+    /// have no more turns left. Note that a turns value of 0 or less means the 
+    /// person has an infinite number of turns. An error exception is thrown 
     /// if the queue is empty.
     /// </summary>
     public Person GetNextPerson()
@@ -37,21 +37,93 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
+
+        Person person = _people.Dequeue();
+
+        // Re-enqueue the person if they have turns left or have infinite turns (turns <= 0)
+        if (person.Turns != 0 && person.Turns != 1)
         {
-            Person person = _people.Dequeue();
             if (person.Turns > 1)
             {
-                person.Turns -= 1;
-                _people.Enqueue(person);
+                person.Turns--;
             }
-
-            return person;
+            _people.Enqueue(person);
         }
+
+        return person;
     }
 
     public override string ToString()
     {
         return _people.ToString();
+    }
+}
+
+/// <summary>
+/// A basic implementation of a Queue.
+/// </summary>
+public class PersonQueue
+{
+    private readonly List<Person> _queue = new();
+
+    public int Length => _queue.Count;
+
+    /// <summary>
+    /// Add a person to the queue.
+    /// </summary>
+    /// <param name="person">The person to add.</param>
+    public void Enqueue(Person person)
+    {
+        _queue.Add(person); // Add to the end of the list
+    }
+
+    /// <summary>
+    /// Remove a person from the queue.
+    /// </summary>
+    /// <returns>The person removed from the front of the queue.</returns>
+    public Person Dequeue()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Queue is empty.");
+        }
+
+        var person = _queue[0]; // Get the first person
+        _queue.RemoveAt(0); // Remove from the front
+        return person;
+    }
+
+    /// <summary>
+    /// Check if the queue is empty.
+    /// </summary>
+    /// <returns>True if the queue is empty; otherwise, false.</returns>
+    public bool IsEmpty()
+    {
+        return _queue.Count == 0;
+    }
+
+    public override string ToString()
+    {
+        return $"[{string.Join(", ", _queue)}]";
+    }
+}
+
+/// <summary>
+/// Represents a person in the queue with a name and a number of turns.
+/// </summary>
+public class Person
+{
+    public string Name { get; private set; }
+    public int Turns { get; set; }
+
+    public Person(string name, int turns)
+    {
+        Name = name;
+        Turns = turns;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Turns})";
     }
 }

44611ee7992377b2020c458dac366a7f735dfc61
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-11-20 21:29:08 -0600

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,17 @@
-public class FeatureCollection
-{
-    // TODO Problem 5 - ADD YOUR CODE HERE
+
+    public class FeatureCollection {
+    // Todo Earthquake Problem - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    public List<Feature> Features { get; set; }
+}
+public class Feature
+{
+    public string Id { get; set; }
+    public FeatureProperties Properties { get; set; }
+}
+public class FeatureProperties
+{
+    public string Place { get; set; }
+    public decimal Mag { get; set; }
+}
 }

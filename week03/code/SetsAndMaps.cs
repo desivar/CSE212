a432d6192a7805faaa8ad22b6f2194b132e61649
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-12-04 00:43:17 -0600

    Update SetsAndMaps.cs

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -82,22 +82,5 @@ public static class SetsAndMaps
     }
 }
 
-public class FeatureCollection
-{
-    public List<Feature> Features { get; set; }
-}
 
-public class Feature
-{
-    public Properties Properties { get; set; }
-}
-
-public class Properties
-{
-    public string Place { get; set; }
-    public double? Mag { get; set; }
-}
-
-   
-   
    

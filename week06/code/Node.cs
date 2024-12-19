dbce0b6200812581c435264126ccb905c0a2d6e0
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-12-18 22:01:05 -0600

    Update Node.cs

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,77 +1,81 @@
-public class Node
-{
+public class Node {
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    // Constructor to initialize the node
-    public Node(int data)
-    {
+    public Node(int data) {
         this.Data = data;
-        this.Right = null;
-        this.Left = null;
     }
 
-    /// <summary>
-    /// Inserts a value into the binary search tree.
-    /// </summary>
-    /// <param name="value">The value to insert.</param>
-    public void Insert(int value)
-    {
-        if (value < Data)
-        {
+    public void Insert(int value) {
+        // if value already exists the skip it
+        if (value == Data)
+            return;
+
+        if (value < Data) {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else if (value > Data)
-        {
+        else {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
-        // Duplicates are ignored
     }
 
-    /// <summary>
-    /// Checks whether the tree contains a specific value.
-    /// </summary>
-    /// <param name="value">The value to check for.</param>
-    /// <returns>True if the value exists in the tree, false otherwise.</returns>
-    public bool Contains(int value)
-    {
-        if (value == Data)
-        {
+    public bool Contains(int value) {
+        // TODO Start Problem 2
+        
+        // if value matches current node's return true
+        if (value == Data) 
             return true;
+
+        if (value < Data) {
+            // Check value on the left
+            return Left is null ? false : Left.Contains(value);
+            
         }
-        else if (value < Data)
-        {
-            return Left != null && Left.Contains(value);
-        }
-        else // value > Data
-        {
-            return Right != null && Right.Contains(value);
+        else {
+            // Check value ion the right
+            return Right is null ? false : Right.Contains(value);
         }
     }
 
-    /// <summary>
-    /// Gets the height of the binary search tree.
-    /// </summary>
-    /// <returns>The height of the tree.</returns>
-    public int GetHeight()
-    {
-        int leftHeight = Left?.GetHeight() ?? 0; // Recursively get left subtree height
-        int rightHeight = Right?.GetHeight() ?? 0; // Recursively get right subtree height
+    public int GetHeight(int size = 0) {
+        // TODO Start Problem 4
+        
+        // Increase size by one
+        size++;
 
-        return Math.Max(leftHeight, rightHeight) + 1; // Height is the maximum of both subtrees plus 1
+        // variables to store size of subtree
+        int leftHeigh = 0, rightHeigh = 0;
+
+        // if no more leaves or children return current value
+        if(Left == null && Right == null)
+        {
+            return size;
+        }
+       
+        // get size of left subtree
+        if(Left is not null) {
+            leftHeigh = Left.GetHeight(size);
+        }
+
+        // get size of right subtree
+        if(Right is not null) {
+            rightHeigh = Right.GetHeight(size);
+        }
+
+        // return value of tallest branch
+        if(leftHeigh > rightHeigh) 
+            return leftHeigh;
+        else 
+            return rightHeigh;
+       
     }
 }
-
-       
-   
-     
-        

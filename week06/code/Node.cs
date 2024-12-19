24cc8204e79ca168f88d6033a22ce45a45c3cc07
Desire Delmy Vargas Tinoco <135285744+desivar@users.noreply.github.com>
2024-12-18 22:05:08 -0600

    Update Node.cs

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,25 +1,30 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
-        this.Data = data;
+    public Node(int data)
+    {
+        Data = data;
     }
 
-    public void Insert(int value) {
-        // if value already exists the skip it
+    public void Insert(int value)
+    {
+        // if value already exists then skip it
         if (value == Data)
             return;
 
-        if (value < Data) {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -28,54 +33,55 @@ public class Node {
         }
     }
 
-    public bool Contains(int value) {
-        // TODO Start Problem 2
-        
+    public bool Contains(int value)
+    {
         // if value matches current node's return true
-        if (value == Data) 
+        if (value == Data)
             return true;
 
-        if (value < Data) {
+        if (value < Data)
+        {
             // Check value on the left
             return Left is null ? false : Left.Contains(value);
-            
         }
-        else {
-            // Check value ion the right
+        else
+        {
+            // Check value on the right
             return Right is null ? false : Right.Contains(value);
         }
     }
 
-    public int GetHeight(int size = 0) {
-        // TODO Start Problem 4
-        
+    public int GetHeight(int size = 0)
+    {
         // Increase size by one
         size++;
 
         // variables to store size of subtree
-        int leftHeigh = 0, rightHeigh = 0;
+        int leftHeight = 0, rightHeight = 0;
 
         // if no more leaves or children return current value
-        if(Left == null && Right == null)
+        if (Left == null && Right == null)
         {
             return size;
         }
-       
+
         // get size of left subtree
-        if(Left is not null) {
-            leftHeigh = Left.GetHeight(size);
+        if (Left is not null)
+        {
+            leftHeight = Left.GetHeight(size);
         }
 
         // get size of right subtree
-        if(Right is not null) {
-            rightHeigh = Right.GetHeight(size);
+        if (Right is not null)
+        {
+            rightHeight = Right.GetHeight(size);
         }
 
         // return value of tallest branch
-        if(leftHeigh > rightHeigh) 
-            return leftHeigh;
-        else 
-            return rightHeigh;
-       
+        return leftHeight > rightHeight ? leftHeight : rightHeight;
     }
 }
+
+
+    
+        
